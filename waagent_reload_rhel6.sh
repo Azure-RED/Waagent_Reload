2d368a0893c2d6fa18f4a461fe8e49e0ce3e99d0
Richard Eseke <v-riesek@microsoft.com>
2017-08-13 19:57:01 -0700

    logger update to RH scripts

--- waagent_reload_rhel6.sh
+++ waagent_reload_rhel6.sh
@@ -12,43 +12,47 @@
 
 # Test for root or sudo
 if [[ "$(id -u)" -ne "$ROOTUID" ]] ; then
-        echo "This script must be executed with super-user privileges."
+        logger -s "This script must be executed with super-user privileges."
         exit 1
 fi
 
 # Check for systemctl
 if [ ! -e /sbin/service ]; then
-        echo "Wrong OS verion, missing service utility."
-        exit 1
+        logger -s"Wrong OS verion, missing service utility."
+        exit 2
 fi
 
 sudo service waagent stop
+echo "Please wait."
 sleep 1
 
 # Remove the waagent (force if needed)
 yum remove WALinuxAgent.noarch -y
-echo "** Wait for completion. **"
-sleep 15
+logger "** Wait for completion. **"
+sleep 5
 
 # Double check to see if the binary is gone.
 if [ -e /usr/sbin/waagent ]; then
         rm /usr/sbin/waagent*
-        echo "Waagent found: Removing waagent binary"
+        logger "Waagent found: Removing waagent binary"
 fi
- 
+
 # Now, install the waagent
 yum install WALinuxAgent.noarch -y
-echo "** Wait for completion. **"
-sleep 15
+logger "** Wait for completion. **"
+sleep 5
 
 # Lastly verify that the waagent is active
 service waagent start
-echo `(service waagent status | grep running)`
+logger $(service waagent status | grep running)
 sleep 1
 
 # When the agent is up and running then enable it to start on boot.
 # Make sure waagent is on.
-chkconfig waagent on
+logger $(chkconfig waagent on)
 
 # Verify that the waagent is enabled for the needed runlevels
-chkconfig | grep waagent
+logger $(chkconfig | grep waagent)
+Version=$(waagent -version | grep Agent | cut -c1-19)
+logger "$Version installed"
+echo "Reinstall Script Complete."
--- waagent_reload_rhel7.sh
+++ waagent_reload_rhel7.sh
@@ -10,14 +10,14 @@
 
 # Test for root or sudo
 if [[ "$(id -u)" -ne "$ROOTUID" ]] ; then
-	echo "This script must be executed with super-user privileges."
+	logger "This script must be executed with super-user privileges."
 	exit 1
 fi
 
 # check for systemctl
 if [ ! -e /bin/systemctl ]; then
-        echo "Wrong OS verion, missing systemctl."
-        exit 2
+	logger "Wrong OS verion, missing systemctl."
+	exit 2
 fi
 
 # Stop and wait for waagent to stop
@@ -27,25 +27,26 @@ sleep 1
 # Remove the waagent
 yum remove WALinuxAgent.noarch -y
 echo "** Wait for completion. **"
-sleep 15
+sleep 5
 
 # Double check to see if the binary is gone.
 if [ -e /usr/sbin/waagent ]; then
         rm /usr/sbin/waagent*
-        echo "Waagent found: Removing waagent binary"
+        logger "Waagent found: Removing waagent binary"
 fi
  
 # Now install the waagent
 yum install WALinuxAgent.noarch -y
 echo "** Wait for completion. **"
-sleep 15
+sleep 1
 
 # Lastly verify that the waagent is active
 systemctl start waagent
-echo `(systemctl status waagent | grep active)`
+logger  $(systemctl status waagent | grep active)
 sleep 1
 
 # When the agent is up and running then enable it to start on boot.
 systemctl enable waagent
-
+Version=$(waagent -version | grep Agent | cut -c1-19)
+logger "$Version installed"
 # End
